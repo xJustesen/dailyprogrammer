351557ddf633b4a03696ec66be391cf3997e97f9
xjustesen <jensbojustesen@gmail.com>
2019-01-15 16:00:37 +0100

    Initial commit

---
+++ 372/challenge-372.py
@@ -0,0 +1,62 @@
+def check_string(STR, ID):
+    '''
+    Given a string (STR) containing only lowercase letters (ID), this function finds whether every letter that appears in the string appears the same number of times.
+    An empty list is handled as TRUE
+    '''
+
+    SIZE = len(ID)
+    counts = [0] * SIZE
+
+    # Count number of time a letter appears
+    for i in range(SIZE):
+        id = ID[i]
+        for l in STR:
+            if id == l:
+                counts[i] += 1
+
+    return len(set(counts)) <= 1
+
+def output(STR, ID):
+    ''' Writes output from "check_string" to terminal '''
+    if check_string(STR, ID):
+        print("The string: '" + STR + "' is balanced.")
+    else:
+        print("The string: '" + STR + "' is not balanced.")
+
+def getLetters(STR):
+    ''' Given a string (STR) containing only lowercase letters, this function returns a list of which lowercase letters (ID) appear in the string '''
+    if len(STR) > 0:
+        ID = [STR[0]]
+
+        for l in STR:
+            for id in ID:
+                isUnique = False if l == id else True
+                if isUnique == False: break
+            if isUnique:
+                ID.append(l)
+    else:
+        ID = STR
+
+    return ID
+
+def main():
+
+    balanced = "xxxyyy"
+    notBalanced = "xxxyy"
+    empty = ""
+    single = "x"
+    mixed = "aaaxxafxbcxhefqf"
+
+    ID = getLetters(balanced);
+    output(balanced, ID)
+    ID = getLetters(notBalanced);
+    output(notBalanced, ID)
+    ID = getLetters(empty);
+    output(empty, ID)
+    ID = getLetters(single);
+    output(single, ID)
+    ID = getLetters(mixed);
+    output(mixed, ID)
+
+if __name__ == '__main__':
+    main()
